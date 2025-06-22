611d2193282e496a0cde319b3b93bede5afd7f91
JensenJ <13812018+JensenJ@users.noreply.github.com>
2025-06-22 13:43:49 +0100

    Whip Changes
    Updated whip name, and tooltip.
    Added recipe, advancement trigger for book.
    Targeted villager is now tracked by the whip in data, there is no visual representation of this yet though.

--- common/src/main/java/net/motivationinnovation/WhipItem.java
+++ common/src/main/java/net/motivationinnovation/WhipItem.java
@@ -1,13 +1,17 @@
 package net.motivationinnovation;
 
+import java.util.List;
 import java.util.Optional;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.GlobalPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
@@ -17,16 +21,35 @@ import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 
 import dev.architectury.event.EventResult;
-
-// TODO: Add tooltip
-// TODO: Fix third person orientation - maybe make this derive from a weapon for easy fix
-// TODO: Add recipe + recipe book advancement
+import org.jetbrains.annotations.NotNull;
 
 public class WhipItem extends Item {
+
+    private Villager targetedVillager = null;
+
     public WhipItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext useOnContext) {
+        if (targetedVillager == null) {
+            return InteractionResult.PASS;
+        }
+
+        if (targetedVillager.isDeadOrDying()) {
+            return InteractionResult.PASS;
+        }
+
+        if (targetedVillager.touchingUnloadedChunk()) {
+            return InteractionResult.PASS;
+        }
+
+        return InteractionResult.PASS;
     }
 
     public static EventResult handleWhipInteraction(Player player, Entity entity, InteractionHand hand) {
@@ -50,6 +73,8 @@ public class WhipItem extends Item {
         if (!player.isShiftKeyDown()) {
             return tryRefreshVillagerTrades(player, villager, whipItem);
         }
+
+        whipItem.setTargetedVillager(villager);
 
         Brain<Villager> brain = villager.getBrain();
         Optional<GlobalPos> jobSite = brain.getMemory(MemoryModuleType.JOB_SITE);
@@ -90,8 +115,18 @@ public class WhipItem extends Item {
                             SoundSource.NEUTRAL,
                             0.5F,
                             player.level().getRandom().nextFloat() / 2);
+        } else {
+            player.level()
+                    .playSound(
+                            null,
+                            player.getX(),
+                            player.getY(),
+                            player.getZ(),
+                            SoundEvents.VILLAGER_HURT,
+                            SoundSource.NEUTRAL,
+                            0.5F,
+                            0.4F / (player.level().getRandom().nextFloat() * 0.4F + 0.8F));
         }
-        // TODO: Get whip sound effect
         player.level()
                 .playSound(
                         null,
@@ -104,5 +139,19 @@ public class WhipItem extends Item {
                         0.4F / (player.level().getRandom().nextFloat() * 0.4F + 0.8F));
 
         return EventResult.pass();
+    }
+
+    @Override
+    public void appendHoverText(
+            ItemStack itemStack, TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag) {
+        list.add(Component.translatable("tooltip.motivationinnovation.whip").withStyle(ChatFormatting.DARK_PURPLE));
+    }
+
+    public void setTargetedVillager(Villager villager) {
+        this.targetedVillager = villager;
+    }
+
+    public Villager getTargetedVillager() {
+        return this.targetedVillager;
     }
 }
